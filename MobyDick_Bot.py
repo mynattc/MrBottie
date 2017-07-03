65cd82b03ea3bbddc5f993610a6cb1b2cbf2a871
Chris Mynatt <mynattchris@gmail.com>
2017-07-02 20:30:16 -0500

    Updated script to include auto reply to tweets

--- MobyDick_Bot.py
+++ MobyDick_Bot.py
@@ -3,18 +3,13 @@ import os, markovchain
 
 #initialize Bottie
 bottie= markovchain()
-
 # Get the current directory's path
 dirname = os.path.dirname(os.path.abspath(__file__))
-
 # Construct the path to the book
 book = os.path.join(dirname, 'mobydick.txt')
-
 # Make your bot read the book!
 bottie.read(book)
-
 #----------------------------------------------
-
 # Consumer Key (API Key)
 cons_key = ''
 
@@ -29,7 +24,6 @@ access_token_secret = ''
 
 # Log in to Twitter
 tweetbot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
-
 #----------------------------------------------
 """
 # See if Bottie is outputting text
@@ -37,6 +31,19 @@ my_first_text = bottie.generate_text(25, seedword=['whale', 'ship'])
 print("MrBottie says:")
 print(my_first_text)
 """
+
+# The target string is what the bot will reply to on Twitter. 
+targetstring = 'MobyDickAhoy!'
+"""Keywords are words the bot will look for in tweets it'll reply to, and it
+will attempt to use them as seeds for the reply"""
+keywords = ['whale', 'Moby Dick', 'ship', 'harpoon', 'water']
+# This is added to the end of any outgoing tweet.
+suffix = '#MrBottie'
+
+# Limits the number of replies allowed by the bot. Default of 2.
+maxconvdepth = 2
+# Trigger autoreply
+tweetbot.twitter_autoreply_start(targetstring, keywords=keywords, prefix=prefix, suffix=suffix, maxconvdepth=maxconvdepth)
 
 # Start periodically tweeting...(forever for now)
 while true:
